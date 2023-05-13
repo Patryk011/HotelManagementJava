9432fe6663b06b438fd446f60d805eaae7726bfd
Patryk <patryknartowski@wp.pl>
2023-05-13 23:30:55 +0200

    fix

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <hello-world></hello-world>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -14,5 +15,12 @@ export default {
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
