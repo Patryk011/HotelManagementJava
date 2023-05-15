e75c1f9c3d395af0378fb689ddfc4acd038b425f
Patryk <patryknartowski@wp.pl>
2023-05-16 01:19:06 +0200

    add styles and main view

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-    <div id="app">
-        <h1>Main View</h1>
-        <button @click="goToCustomers">Go to Customers</button>
-        <button @click="goToRooms">Go to Rooms</button>
+    <div id="app" class="main">
+        <h1>Hotel management</h1>
+
+        <button class="button button-customer" @click="goToCustomers">Go to Customers</button>
+        <button class="button button-room" @click="goToRooms">Go to Rooms</button>
+        <button class="button button-back" @click="goBack">Back</button>
+
         <router-view></router-view>
     </div>
 </template>
@@ -17,10 +20,36 @@ export default {
         goToRooms() {
             this.$router.push('/rooms');
         },
+        goBack() {
+            this.$router.push('/');
+        },
     },
 };
 </script>
 
 <style>
-/* Twoje style CSS tutaj */
+.main {
+    margin: 0 auto;
+    text-align: center;
+    position: relative;
+}
+.button {
+    position: absolute;
+    left: 0;
+}
+
+.button-customer {
+    top: 250px;
+    left: 60px
+}
+
+.button-room {
+    top: 300px;
+    left: 60px
+}
+
+.button-back {
+    top: 350px;
+    left: 60px
+}
 </style>
--- src/frontend/src/components/Room.vue
+++ src/frontend/src/components/Room.vue
@@ -49,4 +49,34 @@ export default {
 
 <style scoped>
 
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+.text-center {
+    text-align: center;
+}
+
+.table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+.table th,
+.table td {
+    padding: 10px;
+    border: 1px solid #ccc;
+}
+
+.table th {
+    background-color: #f0f0f0;
+    font-weight: bold;
+}
+
+.table-striped tbody tr:nth-child(odd) {
+    background-color: #f9f9f9;
+}
+
 </style>
