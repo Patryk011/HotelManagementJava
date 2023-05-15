fa5492ac5d138cb66d6df6559ec389f2a3babe95
Patryk <patryknartowski@wp.pl>
2023-05-16 00:25:28 +0200

    frontend and gitignore update

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,22 +1,26 @@
 <template>
-  <Customer/>
-  <Rooms/>
+    <div id="app">
+        <h1>Main View</h1>
+        <button @click="goToCustomers">Go to Customers</button>
+        <button @click="goToRooms">Go to Rooms</button>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-import Customer from "@/components/Customer.vue";
-import Rooms from "@/components/Room.vue";
-
 export default {
     name: 'App',
-    components: {
-        Rooms,
-        Customer
-    }
-
-}
+    methods: {
+        goToCustomers() {
+            this.$router.push('/customers');
+        },
+        goToRooms() {
+            this.$router.push('/rooms');
+        },
+    },
+};
 </script>
 
 <style>
-
+/* Twoje style CSS tutaj */
 </style>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import Customer from './components/Customer.vue';
+import Room from './components/Room.vue';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/customers', component: Customer },
+    { path: '/rooms', component: Room },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
