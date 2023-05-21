b08ea50a760fb05ce9b8768b53ca187989dd8521
Patryk <patryknartowski@wp.pl>
2023-05-21 15:31:33 +0200

    webconfig

---
+++ src/main/java/pl/project/security/WebConfig.java
@@ -0,0 +1,20 @@
+package pl.project.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/customers").setViewName("forward:/");
+        registry.addViewController("/customers/*").setViewName("forward:/");
+        registry.addViewController("/hotel").setViewName("forward:/");
+        registry.addViewController("/hotel/*").setViewName("forward:/");
+        registry.addViewController("/rooms").setViewName("forward:/");
+        registry.addViewController("/rooms/*").setViewName("forward:/");
+    }
+}
