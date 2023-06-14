6e69db8805cbd74a32109e52a2850d3f8954d794
Patryk <patryknartowski@wp.pl>
2023-06-14 11:09:46 +0200

    delete useless files

--- src/main/java/pl/project/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.project.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/pl/project/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.project.entity.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
