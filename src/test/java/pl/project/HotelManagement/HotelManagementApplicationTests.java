e999133ff83e84c4069924c80fedb5f1f5c8cef4
Patryk <patryknartowski@wp.pl>
2023-06-16 22:04:26 +0200

    final

--- src/test/java/pl/project/HotelManagement/HotelManagementApplicationTests.java
+++ src/test/java/pl/project/HotelManagement/HotelManagementApplicationTests.java
@@ -1,11 +1,20 @@
 package pl.project.HotelManagement;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import pl.project.email.EmailReceiver;
 
 @SpringBootTest
 class HotelManagementApplicationTests {
 
+	@MockBean
+	private JavaMailSender javaMailSender;
+
+	@MockBean
+	private EmailReceiver emailReceiver;
 	@Test
 	void contextLoads() {
 	}
